5b3ccb07c2ac9881450f3bf91133428b9ebc5b3b
Chase <cc7768@gmail.com>
2018-11-07 00:24:00 -0500

    Kind of working

--- code/static_oracle_problem.jl
+++ code/static_oracle_problem.jl
@@ -7,13 +7,13 @@ using NLopt
 using Parameters
 using Random
 
-struct Params
+struct Params{D<:Distribution}
     # Number of voters
     K::Int
 
     # Parameters for distribution
     μ_i::Float64
-    i::Distribution
+    i::D
 
     # Malevolent/Oracle parameters
     ξm::Float64
@@ -21,40 +21,39 @@ struct Params
 end
 
 
-function voter_payoffs(ap::Params, x, Z, S, i, ε, εtilde)
+function voter_payoffs(ap::Params, x, Z, i, ε, εtilde)
     @unpack K = ap
 
     p_prime = ifelse(Z > round(Int, K/2), 0.0, 1.0)
-    tt_bonus = ifelse(x==S, i, 0.0)
+    tt_bonus = ifelse(x==1, i, 0.0)
 
-    return p_prime*ε[x+1, Z+1] + εtilde[x+1, Z+1, S+1] + tt_bonus
+    return p_prime*ε[Z+1, x+1] + εtilde[Z+1, x+1] + tt_bonus
 end
 
 
-function find_xstar(ap::Params, S, i, ε, εtilde, π)
+function find_xstar(ap::Params, i, ε, εtilde, π)
 
     @unpack K = ap
 
     # Set x0/x1 to 0
     x0, x1 = (0.0, 0.0)
     for z in 0:K
-        x0 += voter_payoffs(ap, 0, z, S, i, ε, εtilde) * π[z+1]
-        x1 += voter_payoffs(ap, 1, z, S, i, ε, εtilde) * π[z+1]
+        x0 += voter_payoffs(ap, 0, z, i, ε, εtilde) * π[z+1]
+        x1 += voter_payoffs(ap, 1, z, i, ε, εtilde) * π[z+1]
     end
 
     return ifelse(x0 > x1, (0, x0), (1, x1))
 end
 
 
-function find_πz(ap::Params, S, ε, εtilde, nSamples=10_000)
+function find_πz(ap::Params, ε, εtilde, nSamples=10_000)
     @unpack K, μ_i, i = ap
 
     π_0 = ones(K+1) ./ (K+1)
     π_update = Array{Float64}(undef, K+1)
 
     dist, iter = 1.0, 0
-    while dist > 1e-6
-
+    while dist > 1e-3
         iter += 1
 
         z_count = zeros(Int, K+1)
@@ -66,7 +65,9 @@ function find_πz(ap::Params, S, ε, εtilde, nSamples=10_000)
             for _i in i_vals
 
                 # If telling truth, increment truth
-                if find_xstar(ap, S, _i, ε, εtilde, π_0)[1] == 1
+                # TODO: Can read ibar from formulas... Maybe can compute this
+                #       faster
+                if find_xstar(ap, _i, ε, εtilde, π_0)[1] == 1
                     n_x1 += 1
                 else
                     n_x0 += 1
@@ -75,7 +76,7 @@ function find_πz(ap::Params, S, ε, εtilde, nSamples=10_000)
             z_count[n_x0+1] += 1
         end
 
-        π_update = z_count / nSamples
+        π_update = z_count ./ nSamples
         dist = maximum(abs, π_update - π_0)
         copyto!(π_0, π_update)
 
@@ -90,27 +91,27 @@ function find_πz(ap::Params, S, ε, εtilde, nSamples=10_000)
 end
 
 
-function malevolent_cost(p::Params, S, ε, εtildeV)
+function malevolent_cost(p::Params, ε, εtilde)
     # Unpack parameters
     @unpack μ_i, i, K, ξm = p
 
     # Given new εtilde, determine distribution of z
-    πz = find_πz(p, S, ε, εtilde)
+    πz = find_πz(p, ε, εtilde)
 
     E_cost = 0.0
     for z in 0:K
-        E_cost += z * εtilde[1-S + 1, z+1, S+1] * πz[z+1]
+        E_cost += z * εtilde[z+1, 0+1] * πz[z+1]
     end
 
     return E_cost
 end
 
 
-function lie_constraint(p::Params, S, ε, εtildeV)
+function lie_constraint(p::Params, ε, εtilde)
     @unpack K, ξm = p
 
     # Compute the pmf and cmf for Zs
-    πz = find_πz(p, S, ε, εtilde)
+    πz = find_πz(p, ε, εtilde)
     Fz = cumsum(πz)
 
     # Figure out where to evaluate cmf and return
@@ -120,31 +121,41 @@ end
 
 
 function optimize_εtilde(p::Params, ε)
-    let p=p, ε=ε
-        myobj(εtilde::Vector, grad::Vector) = malevolent_cost(p, 1, ε, εtilde)
-        mycons(εtilde::Vector, grad::Vector) = lie_constraint(p, 1, ε, εtilde)
-    end
 
     # Set up optimization
-    opt = Opt(:LN_NELDERMEAD, ?)
+    opt = Opt(:LN_COBYLA, p.K + 1)
 
-    lower_bounds!(opt, zeros())
-    min_objective!(opt, myobj)
-    inequality_constraint!(opt, mycons, 1e-8)
+    let p=p, ε=ε
 
-    minf, minx, ret = optimize(opt, ?)
+        function myobj(εtilde::Vector, grad::Vector)
+            εtildeM = hcat(εtilde, zeros(size(εtilde)))
+            return malevolent_cost(p, ε, εtildeM)
+        end
+
+        function mycons(εtilde::Vector, grad::Vector)
+            εtildeM = hcat(εtilde, zeros(size(εtilde)))
+            return lie_constraint(p, ε, εtildeM)
+        end
+
+
+        lower_bounds!(opt, zeros(p.K + 1))
+        min_objective!(opt, myobj)
+        inequality_constraint!(opt, mycons, 1e-8)
+    end
+
+    minf, minx, ret = optimize(opt, zeros(p.K + 1))
+
     return opt, minf, minx, ret
 end
 
+
 #=
-ε = ones(2, 101)
-εtilde = zeros(2, 101, 2)
-εtilde[1, :, :] = 3*zeros(101, 2)
+ε = ones(101, 2)
+εtilde = zeros(101, 2)
+εtilde[1, :] = ones(101)
 
 p = Params(100, 1.0, Normal(0, 0.5), 0.75, 0.85)
 
-πz = find_πz(p, 1, ε, εtilde)
-
-mp = MalevolentParams(0.75)
+πz = find_πz(p, ε, εtilde)
 
 =#
