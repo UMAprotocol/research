81e97351f2780ca4710623a5bf2821f6e03ee859
Chase <cc7768@gmail.com>
2019-08-29 11:34:33 -0400

    Add stochastic growth process

--- code/taxation_problem.jl
+++ code/taxation_problem.jl
@@ -28,14 +28,13 @@ end
 
 function find_tax_policy(
         model::TaxModel, Xstar=compute_state_dividend_payments(model);
-        tol::Float64=1e-10, maxiter::Int=10_000, n_howard_steps::Int=25
+        nD::Int=500, tol::Float64=1e-10, maxiter::Int=10_000, n_howard_steps::Int=25
     )
     # Unpack parameters
     @unpack γ, r, M = model
     nM = length(M.state_values)
 
     # Create a savings grid
-    nD = 500
     D = range(0.0, maximum(M.state_values), length=nD)
 
     # Allocate space for policy and value functions
@@ -118,14 +117,98 @@ function find_tax_policy(
         end
     end
 
-    return iDstar, Tstar, Xstar, Vstar
+    return iDstar, Tstar, Xstar, D, Vstar
 end
 
+
+# Mtp1 = Mt + g Mt (1 - Mt/Mbar)
+function create_scurve_mc(M0=1.0, Mbar=1000.0, g=0.5, σ=2.5, nM=499)
+    # Create evenly spaced points between M0 and Mbar
+    M_values = collect(range(M0, Mbar*(1 + σ), length=nM))
+
+    # Allocate space for transition matrix and fill
+    # by determining transition probabilities
+    P = zeros(nM, nM)
+    for iM_t in 1:nM
+        # Pull out today's margin
+        M_t = M_values[iM_t]
+
+        # Compute expected value and std deviation of margin
+        # tomorrow and create distribution over t+1 values
+        E_M_tp1 = M_t * (1 + g*(1.0 - M_t/Mbar))
+        std_dev = M_t*σ
+        d_M_tp1 = Normal(E_M_tp1, std_dev)
+
+        for iM_tp1 in 1:nM
+            # How much did we deviate from our expectation
+            lb = iM_tp1 == 1 ? -Inf : M_values[iM_tp1 - 1]
+            ub = iM_tp1 == nM ? Inf : M_values[iM_tp1]
+
+            # Fill in probability
+            P[iM_t, iM_tp1] = cdf(d_M_tp1, ub) - cdf(d_M_tp1, lb)
+        end
+    end
+
+    return MarkovChain(P, M_values)
+end
+
+
+function create_scurve_mc(M0=1.0, Mbar=1000.0, g=0.5, σ=2.5, nM=499)
+    # Create evenly spaced points between M0 and Mbar
+    M_values = collect(range(M0, Mbar*(1 + σ), length=nM))
+
+    # Allocate space for transition matrix and fill
+    # by determining transition probabilities
+    P = zeros(nM, nM)
+    for iM_t in 1:nM
+        # Pull out today's margin
+        M_t = M_values[iM_t]
+
+        # Compute expected value and std deviation of margin
+        # tomorrow and create distribution over t+1 values
+        E_M_tp1 = M_t * (1 + g*(1.0 - M_t/Mbar))
+        std_dev = σ
+        d_M_tp1 = Normal(E_M_tp1, std_dev)
+
+        for iM_tp1 in 1:nM
+            # How much did we deviate from our expectation
+            lb = iM_tp1 == 1 ? -Inf : M_values[iM_tp1 - 1]
+            ub = iM_tp1 == nM ? Inf : M_values[iM_tp1]
+
+            # Fill in probability
+            P[iM_t, iM_tp1] = cdf(d_M_tp1, ub) - cdf(d_M_tp1, lb)
+        end
+    end
+
+    return MarkovChain(P, M_values)
+end
 #=
+# Toy example
+μ = 1000.0
+σ = 50.0
+mc_p = MarkovChain([0.95 0.05 0.0; 0.025 0.95 0.025; 0.025 0.025 0.95], [μ - σ, μ, μ + σ])
+model_p = TaxModel(;r=0.00206, M=mc_p)
+iDstar, Tstar, Xstar, D, Vstar = find_tax_policy(model_p; nD=1000, tol=1e-3, n_howard_steps=10)
+
 # Persistent model
-mc_p = rouwenhorst(7, 0.95, sqrt(0.01 * (1.0 - 0.95^2)), 1.0*0.05)
+μ = 1000.0
+σ = 15.0
+mc_p = rouwenhorst(7, 0.999, sqrt(σ * (1.0 - 0.999^2)), μ*0.001)
 model_p = TaxModel(;M=mc_p)
+iDstar, Tstar, Xstar, D, Vstar = find_tax_policy(model_p; nD=2500, tol=1e-3, n_howard_steps=10)
+
+# Persistent model
+μ = 1000.0
+σ = 250.0
+mc_p = rouwenhorst(5, 0.95, sqrt(σ * (1.0 - 0.95^2)), μ*0.05)
+model_p = TaxModel(;r=0.00206, M=mc_p)
+iDstar, Tstar, Xstar, D, Vstar = find_tax_policy(model_p; nD=2500, tol=1e-3, n_howard_steps=10)
 
 mc_np = rouwenhorst(7, 0.05, sqrt(0.01 * (1.0 - 0.05^2)), 1.0*0.95)
 model_np = TaxModel(;M=mc_np)
+
+# Growth model
+mc_states = [
+  0.05, 0.075, 0.10, 0.15, 0.20, 0.25, 0.
+]
 =#
