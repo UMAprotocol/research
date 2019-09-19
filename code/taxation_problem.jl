e4b61fcaa6f8120929bad8087c7ad322c54f8ead
Chase <cc7768@gmail.com>
2019-09-19 16:31:13 -0400

    Add updated taxation code

--- code/taxation_problem.jl
+++ code/taxation_problem.jl
@@ -1,5 +1,11 @@
+using Distributions
+using GLPK
+using Interpolations
+using JuMP
 using LinearAlgebra
 using Parameters
+using PlotlyJS
+using Random
 using QuantEcon
 
 
@@ -13,7 +19,35 @@ using QuantEcon
 end
 
 
-function compute_state_dividend_payments(model::TaxModel)
+function constrained_dividend_payments(model::TaxModel)
+    # Unpack parameters
+    @unpack γ, r, M = model
+    nM = length(M.state_values)
+
+    # Create JuMP optimizer type
+    opt = Model(with_optimizer(GLPK.Optimizer))
+
+    # Create state by state dividend variables
+    @variable(opt, buybacks[1:nM] >= 0)
+
+    # Objective is to minimize costs
+    geosum = inv(I - (1.0/(1 + r) .* M.p))
+    @objective(opt, Min, sum(geosum*buybacks))
+
+    # Constraint is that we need to ensure the system is secure
+    @constraint(opt, 2*γ.*M.state_values .<= geosum*buybacks)
+
+    JuMP.optimize!(opt)
+
+    if JuMP.termination_status(opt) != JuMP.MOI.OPTIMAL
+        error("Non-optimal solution... Investigate further")
+    end
+
+    return JuMP.value.(buybacks)
+end
+
+
+function unconstrained_dividend_payments(model::TaxModel)
     # Unpack parameters
     @unpack γ, r, M = model
     nM = length(M.state_values)
@@ -27,15 +61,15 @@ end
 
 
 function find_tax_policy(
-        model::TaxModel, Xstar=compute_state_dividend_payments(model);
-        nD::Int=500, tol::Float64=1e-10, maxiter::Int=10_000, n_howard_steps::Int=25
+        model::TaxModel, Xstar=constrained_dividend_payments(model);
+        nD::Int=600, tol::Float64=1e-6, maxiter::Int=10_000, n_howard_steps::Int=25
     )
     # Unpack parameters
     @unpack γ, r, M = model
     nM = length(M.state_values)
 
     # Create a savings grid
-    D = range(0.0, maximum(M.state_values), length=nD)
+    D = range(0.0, maximum(M.state_values)/3.0, length=nD)
 
     # Allocate space for policy and value functions
     iDstar = zeros(Int, nD, nM)
@@ -49,6 +83,8 @@ function find_tax_policy(
 
         # Iterate over states and find new new policies
         for iM in 1:nM
+            M_t = M.state_values[iM]
+
             for iD in 1:nD
                 # Probability of each new state
                 p_iM = M.p[iM, :]
@@ -67,7 +103,7 @@ function find_tax_policy(
                     for iMtp1 in 1:nM
                         EV += p_iM[iMtp1] * Vold[iDtp1, iMtp1]
                     end
-                    _V = _T*_T + (1 / (1+r))*EV
+                    _V = (_T - 0.002*M_t)*(_T - 0.002*M_t) + (1 / (1+r))*EV
 
                     # Update if it is better than previous -- We are minimizing
                     # the tax squared
@@ -122,30 +158,37 @@ end
 
 
 # Mtp1 = Mt + g Mt (1 - Mt/Mbar)
-function create_scurve_mc(M0=1.0, Mbar=1000.0, g=0.5, σ=2.5, nM=499)
-    # Create evenly spaced points between M0 and Mbar
-    M_values = collect(range(M0, Mbar*(1 + σ), length=nM))
+function create_scurve_mc(;M0=1.0, Mbar=1000.0, g=0.005, σ0=0.15, σ=0.02, nM=499)
+
+    # Create evenly spaced points between M0 and Mbar with a value at 0
+    M_values = [0.0; collect(range(M0, Mbar, length=nM))]
+    # Create P and add absorbing disaster state
+    P = zeros(nM+1, nM+1)
+    P[1, 1] = 1.0
+    p_disaster = [0.0; 0.01 .* 1 ./ M_values[2:end]]
 
     # Allocate space for transition matrix and fill
     # by determining transition probabilities
-    P = zeros(nM, nM)
-    for iM_t in 1:nM
+    for iM_t in 2:nM+1
         # Pull out today's margin
         M_t = M_values[iM_t]
 
         # Compute expected value and std deviation of margin
         # tomorrow and create distribution over t+1 values
         E_M_tp1 = M_t * (1 + g*(1.0 - M_t/Mbar))
-        std_dev = M_t*σ
+        std_dev = σ0 + σ*M_t
         d_M_tp1 = Normal(E_M_tp1, std_dev)
 
-        for iM_tp1 in 1:nM
+        # Add disaster risk if system is smaller than 50
+        P[iM_t, 1] = p_disaster[iM_t] ./ (1.0 + p_disaster[iM_t])
+
+        for iM_tp1 in 2:nM+1
             # How much did we deviate from our expectation
-            lb = iM_tp1 == 1 ? -Inf : M_values[iM_tp1 - 1]
-            ub = iM_tp1 == nM ? Inf : M_values[iM_tp1]
+            lb = iM_tp1 == 2 ? -Inf : M_values[iM_tp1 - 1]
+            ub = iM_tp1 == nM+1 ? Inf : M_values[iM_tp1]
 
             # Fill in probability
-            P[iM_t, iM_tp1] = cdf(d_M_tp1, ub) - cdf(d_M_tp1, lb)
+            P[iM_t, iM_tp1] = (cdf(d_M_tp1, ub) - cdf(d_M_tp1, lb)) / (1.0 + p_disaster[iM_t])
         end
     end
 
@@ -153,36 +196,134 @@ function create_scurve_mc(M0=1.0, Mbar=1000.0, g=0.5, σ=2.5, nM=499)
 end
 
 
-function create_scurve_mc(M0=1.0, Mbar=1000.0, g=0.5, σ=2.5, nM=499)
-    # Create evenly spaced points between M0 and Mbar
-    M_values = collect(range(M0, Mbar*(1 + σ), length=nM))
+function simulate_logistic_growth(;M0=1.0, Mbar=1000.0, g=0.005, σ0=0.10, σ=0.05, N=1, T=2500)
+    # Allocate memory and set initial value to M0
+    out = Array{Float64}(undef, N, T)
+    out[:, 1] .= M0
 
-    # Allocate space for transition matrix and fill
-    # by determining transition probabilities
-    P = zeros(nM, nM)
-    for iM_t in 1:nM
-        # Pull out today's margin
-        M_t = M_values[iM_t]
-
-        # Compute expected value and std deviation of margin
-        # tomorrow and create distribution over t+1 values
-        E_M_tp1 = M_t * (1 + g*(1.0 - M_t/Mbar))
-        std_dev = σ
-        d_M_tp1 = Normal(E_M_tp1, std_dev)
-
-        for iM_tp1 in 1:nM
-            # How much did we deviate from our expectation
-            lb = iM_tp1 == 1 ? -Inf : M_values[iM_tp1 - 1]
-            ub = iM_tp1 == nM ? Inf : M_values[iM_tp1]
-
-            # Fill in probability
-            P[iM_t, iM_tp1] = cdf(d_M_tp1, ub) - cdf(d_M_tp1, lb)
-        end
+    for t in 2:T, n in 1:N
+        # Mtp1 = Mt + g Mt (1 - Mt/Mbar)
+        Mt = out[n, t-1]
+        σt = σ0 + σ*Mt
+        out[n, t] = min(max(Mt + g*Mt*(1 - Mt/Mbar) + σt*randn(), M0), Mbar)
     end
 
-    return MarkovChain(P, M_values)
+    return out
 end
+
+
+function create_comparison_table(nMs=[500, 1000, 2500, 5000], Ts)
+    # Create space to store data
+    out = Array{Float64}(undef, length(nMs)+1, length(Ts)*2)
+
+    println("Starting true logistic growth simulations")
+    lg_sims = simulate_logistic_growth(;M0=1.0, Mbar=1000.0, g=0.005, σ=0.15, N=500, T=2000)
+    for (i, t) in enumerate(Ts)
+        out[1, 2*i - 1] = mean(lg_sims[:, t])
+        out[1, 2*i] = std(lg_sims[:, t])
+    end
+    out[1, :]
+
+    for (j, nM) in enumerate(nMs)
+        println("Starting simulations for $nM approximation points")
+        mc = create_scurve_mc(;M0=1.0, Mbar=1000.0, g=0.005, σ=0.15, nM=nM)
+        mc_sims = hcat([simulate(mc, 2000; init=1) for _ in 1:500]...)'
+        for (i, t) in enumerate(Ts)
+            out[j+1, 2*i - 1] = mean(mc_sims[:, t])
+            out[j+1, 2*i] = std(mc_sims[:, t])
+        end
+    end
+end
+
 #=
+
+mc = create_scurve_mc(;g=0.030, σ0=0.35, σ=0.025, nM=1500)
+model = TaxModel(0.5, 2.1e-3, mc)
+
+# Create margin growth figure
+nsims = 150
+Random.seed!(61089)
+mc_sims = [simulate(mc, 12*25; init=2) for i in 1:nsims]
+scatters = [
+    scatter(;
+        x=range(0, 25, length=12*25), y=mc_sims[i],
+        opacity=ifelse(mc_sims[i][end] > 1.0, 0.10, 0.35),
+        marker=attr(color="black"),
+        showlegend=false
+    )
+    for i in 1:nsims
+]
+smg_plot = plot(
+    scatters,
+    Layout(
+        title="Margin Growth Process", xaxis_title="Years from Launch",
+        yaxis_title="Millions of Dollars"
+    )
+)
+savefig(smg_plot, "../notes/TaxationPlanImages/StochasticMarginGrowth.png")
+
+# Compute buyback policy
+cdp = constrained_dividend_payments(model)
+
+mc_ind_sims = [simulate_indices(mc, 12*25; init=2) for i in 1:10]
+annualized_buybacks = ((1.0 .+ (cdp ./ model.M.state_values)).^12 .- 1.0) .* 100
+
+abb_bar = [
+    bar(;
+        x=model.M.state_values, y=annualized_buybacks,
+        marker=attr(color="rgb(255, 74, 74)"), showlegend=false
+    )
+]
+
+abb_scatter = [
+    scatter(;
+        x=range(0, 25, length=12*25), y=annualized_buybacks[mc_ind_sims[i]],
+        opacity=0.35, marker=attr(color="black"), showlegend=false
+    )
+    for i in 1:10
+]
+
+abb_bar_plot = plot(
+    abb_bar,
+    Layout(
+        title="Annualized Buyback Percents (as % of Margin)",
+        xaxis_title="System Margin (millions of dollars)",
+        yaxis_range=[0, 20]
+    )
+);
+
+abb_scatter_plot = plot(
+    abb_scatter,
+    Layout(
+        title="Simulated Buyback Percent (as % of Margin)",
+        xaxis_title="Years since launch",
+        yaxis_range=[0, 20]
+    )
+);
+abb_plot = [abb_scatter_plot, abb_bar_plot]
+
+savefig(abb_plot, "../notes/TaxationPlanImages/StochasticBuybacks.png")
+
+iDstar, Tstar, Xstar, D, Vstar = find_tax_policy(
+    model, cdp; nD=500, maxiter=1, n_howard_steps=25
+)
+
+=#
+
+#=
+mc = create_scurve_mc(1.0, 1000.0, 0.025, 15.0, 1000)
+model = TaxModel(0.5, 2.1e-3, mc)
+
+cdp = constrained_dividend_payments(model)
+ucdp = unconstrained_dividend_payments(model)
+
+geosum = inv(I - (1.0/(1 + model.r) .* model.M.p))
+
+[model.M.state_values cdp ucdp]
+[model.M.state_values geosum*[cdp ucdp]]
+
+plot([bar(;y=_x) for _x in [cdp, ucdp]])  # Plots buybacks under constrained/unconstrained
+
 # Toy example
 μ = 1000.0
 σ = 50.0
